ac7c70933611287fc0cbadd28764f0019687e333
AlejoGDOfficial <alejogdoff@gmail.com>
2025-04-17 17:36:22 -0500

    the fuckin' tick sound from psych engine (For SoundTray)

--- source/flixel/system/ui/FlxSoundTray.hx
+++ source/flixel/system/ui/FlxSoundTray.hx
@@ -16,51 +16,29 @@ import openfl.text.AntiAliasType;
 import openfl.text.GridFitType;
 #end
 
-/**
- * The flixel sound tray, the little volume meter that pops down sometimes.
- * Accessed via `FlxG.game.soundTray` or `FlxG.sound.soundTray`.
- */
 class FlxSoundTray extends Sprite
 {
-	/**
-	 * Because reading any data from DisplayObject is insanely expensive in hxcpp, keep track of whether we need to update it or not.
-	 */
 	public var active:Bool;
 
-	/**
-	 * Helps us auto-hide the sound tray after a volume change.
-	 */
 	var _timer:Float;
 
-	/**
-	 * Helps display the volume bars on the sound tray.
-	 */
 	var _bars:Array<Bitmap>;
 
-	/**
-	 * How wide the sound tray background is.
-	 */
 	var _width:Int = 80;
 
 	var _defaultScale:Float = 2.0;
 
-	/**The sound used when increasing the volume.**/
-	public var volumeUpSound:String = "flixel/sounds/beep";
+	public var volumeUpSound:String = '';
 
-	/**The sound used when decreasing the volume.**/
-	public var volumeDownSound:String = 'flixel/sounds/beep';
+	public var volumeDownSound:String = '';
 
-	/**Whether or not changing the volume should make noise.**/
-	public var silent:Bool = true;
+	public var silent:Bool = false;
 
 	public var _y:Float;
+
 	public var _alpha:Float = 1;
 
-	/**
-	 * Sets up the "sound tray", the little volume meter that pops down sometimes.
-	 */
-	@:keep
-	public function new()
+	@:keep public function new()
 	{
 		super();
 
@@ -82,17 +60,20 @@ class FlxSoundTray extends Sprite
 		text.embedFonts = true;
 		text.antiAliasType = AntiAliasType.NORMAL;
 		text.gridFitType = GridFitType.PIXEL;
-		#else
 		#end
+
 		var dtf:TextFormat = new TextFormat(Paths.font('rajdhani.ttf'), 10, 0xffffff);
 		dtf.align = TextFormatAlign.CENTER;
 		text.defaultTextFormat = dtf;
-		addChild(text);
 		text.text = "VOLUME";
 		text.y = 16;
 
+		addChild(text);
+
 		var bx:Int = 10;
+
 		var by:Int = 14;
+
 		_bars = new Array();
 
 		for (i in 0...10)
@@ -100,41 +81,39 @@ class FlxSoundTray extends Sprite
 			tmp = new Bitmap(new BitmapData(4, i + 1, false, FlxColor.WHITE));
 			tmp.x = bx;
 			tmp.y = by;
+
 			addChild(tmp);
+
 			_bars.push(tmp);
+
 			bx += 6;
 			by--;
 		}
 
 		y = _y = -height;
+
 		visible = false;
 	}
-
-	/**
-	 * This function updates the soundtray object.
-	 */
-	public function update(MS:Float):Void
+	
+	public function update(elapsed:Float):Void
 	{
 		y = CoolUtil.fpsLerp(y, _y, 0.15);
+
 		alpha = CoolUtil.fpsLerp(alpha, _alpha, 0.15);
 		
-		// Animate sound tray thing
 		if (_timer > 0)
 		{
-			_timer -= (MS / 750);
-		}
-		else if (y > -height)
-		{
+			_timer -= elapsed / 750;
+		} else if (y > -height) {
 			_y = -height;
+
 			_alpha = 0;
 
 			if (y <= -height)
 			{
-				visible = false;
-				active = false;
+				visible = active = false;
 
 				#if FLX_SAVE
-				// Save sound preferences
 				if (FlxG.save.isBound)
 				{
 					FlxG.save.data.mute = FlxG.sound.muted;
@@ -145,52 +124,37 @@ class FlxSoundTray extends Sprite
 			}
 		}
 	}
-
-	/**
-	 * Makes the little volume tray slide out.
-	 *
-	 * @param	up Whether the volume is increasing.
-	 */
+	
 	public function show(up:Bool = false):Void
 	{
 		if (!silent)
-		{
-			var sound = FlxAssets.getSound(up ? volumeUpSound : volumeDownSound);
-			if (sound != null)
-				FlxG.sound.load(sound).play();
-		}
+			FlxG.sound.play(Paths.sound('tick'), 0.75);
 
-		_timer = 1;
+		_timer = _alpha = 1;
+
 		_y = 0;
-		_alpha = 1;
-		visible = true;
-		active = true;
+
+		visible = active = true;
+
 		var globalVolume:Int = Math.round(FlxG.sound.volume * 10);
 
 		if (FlxG.sound.muted)
-		{
 			globalVolume = 0;
-		}
 
 		for (i in 0..._bars.length)
 		{
 			if (i < globalVolume)
-			{
 				_bars[i].alpha = 1;
-			}
 			else
-			{
 				_bars[i].alpha = 0.5;
-			}
 		}
 	}
 
 	public function screenCenter():Void
 	{
-		scaleX = _defaultScale;
-		scaleY = _defaultScale;
+		scaleX = scaleY = _defaultScale;
 
-		x = (0.5 * (Lib.current.stage.stageWidth - _width * _defaultScale) - FlxG.game.x);
+		x = 0.5 * (Lib.current.stage.stageWidth - _width * _defaultScale) - FlxG.game.x;
 	}
 }
 #end
