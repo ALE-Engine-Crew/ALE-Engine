c8ccdf19679f67046be4ecb56ab57d3ddcfa7cda
AlejoGDOfficial <alejogdoff@gmail.com>
2025-05-07 10:58:18 -0500

    Time to Update Flixel

--- source/funkin/visuals/ALECamera.hx
+++ source/funkin/visuals/ALECamera.hx
@@ -5,9 +5,7 @@ class ALECamera extends FlxCamera
 	override public function update(elapsed:Float):Void
 	{
 		if (target != null)
-		{
 			updateFollowDelta(elapsed);
-		}
 
 		updateScroll();
 		updateFlash(elapsed);
@@ -59,31 +57,23 @@ class ALECamera extends FlxCamera
 				edge = targetX + target.width - deadzone.x - deadzone.width;
 
 				if (_scrollTarget.x < edge)
-				{
 					_scrollTarget.x = edge;
-				}
 
 				edge = targetY - deadzone.y;
 
 				if (_scrollTarget.y > edge)
-				{
 					_scrollTarget.y = edge;
-				}
 
 				edge = targetY + target.height - deadzone.y - deadzone.height;
 
 				if (_scrollTarget.y < edge)
-				{
 					_scrollTarget.y = edge;
-				}
 			}
 
-			if ((target is FlxSprite))
+			if (target is FlxSprite)
 			{
 				if (_lastTargetPosition == null)
-				{
-					_lastTargetPosition = FlxPoint.get(target.x, target.y); // Creates this point.
-				}
+					_lastTargetPosition = FlxPoint.get(target.x, target.y);
 
 				_scrollTarget.x += (target.x - _lastTargetPosition.x) * followLead.x;
 				_scrollTarget.y += (target.y - _lastTargetPosition.y) * followLead.y;
@@ -94,6 +84,7 @@ class ALECamera extends FlxCamera
 		}
 
 		var mult:Float = 1 - Math.exp(-elapsed * followLerp);
+		
 		scroll.x += (_scrollTarget.x - scroll.x) * mult;
 		scroll.y += (_scrollTarget.y - scroll.y) * mult;
 	}
